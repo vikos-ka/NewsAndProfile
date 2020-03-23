c74d422f66fcbc4a63863ff2c4874c1399bb1d2e
Vika Kaplunova <vika.vkaplunova@gmail.com>
2020-03-23 10:27:53 +0200

    Updates

---
+++ precache-manifest.cab6067d1d44b2477224f49de9e37aad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a447ec05c2f306023a009a43ee178dff",
+    "url": "/NewsAndProfile/index.html"
+  },
+  {
+    "revision": "87eba503f29052dd64cf",
+    "url": "/NewsAndProfile/static/css/main.fd7ff77d.chunk.css"
+  },
+  {
+    "revision": "2d4adc1576b8271998d3",
+    "url": "/NewsAndProfile/static/js/2.2c1001dc.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/NewsAndProfile/static/js/2.2c1001dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87eba503f29052dd64cf",
+    "url": "/NewsAndProfile/static/js/main.5d749dab.chunk.js"
+  },
+  {
+    "revision": "e47806daf891933e4840",
+    "url": "/NewsAndProfile/static/js/runtime-main.fd7b28b2.js"
+  },
+  {
+    "revision": "144c71668ca9376a96d10bb2774eee93",
+    "url": "/NewsAndProfile/static/media/loader.144c7166.svg"
+  },
+  {
+    "revision": "a9210b592313272532e72c62062e43a0",
+    "url": "/NewsAndProfile/static/media/user.a9210b59.png"
+  }
+]);
